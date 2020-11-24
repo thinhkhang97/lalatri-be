cb8ba18e8124665263e3d4215a3ad78bccc89d5c
thinhkhang97 <thinhkhang97@gmail.com>
2020-11-24 22:13:19 +0700

    authentication: add token table

--- migrations/sqls/20201124144615-authentication-down.sql
+++ migrations/sqls/20201124144615-authentication-down.sql
@@ -4,3 +4,10 @@ DROP TABLE public."user";
 -- public.user_id_seq definition
 
 DROP SEQUENCE public.user_id_seq;
+
+-- Table: public.token
+DROP TABLE public.token;
+
+-- public.token_seq definition
+
+DROP SEQUENCE public.token_seq;
--- migrations/sqls/20201124144615-authentication-up.sql
+++ migrations/sqls/20201124144615-authentication-up.sql
@@ -1,3 +1,4 @@
+-- Create user table
 CREATE SEQUENCE public.user_id_seq
 	INCREMENT BY 1
 	MINVALUE 1
@@ -18,3 +19,32 @@ CREATE TABLE public. "user" (
 TABLESPACE pg_default;
 
 ALTER TABLE public. "user" OWNER TO postgres;
+
+-- Create token table
+CREATE SEQUENCE public.token_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+CREATE TABLE public.token
+(
+    id bigint NOT NULL DEFAULT nextval('token_id_seq'::regclass),
+    user_id bigint NOT NULL,
+    type character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    token character varying(512) COLLATE pg_catalog."default" NOT NULL,
+    created_date timestamp with time zone NOT NULL DEFAULT now(),
+    modified_date timestamp with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT token_pkey PRIMARY KEY (id),
+    CONSTRAINT token_user_fkey FOREIGN KEY (user_id)
+        REFERENCES public."user" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.token
+    OWNER to postgres;
