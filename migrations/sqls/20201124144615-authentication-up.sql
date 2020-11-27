e26a14ebc3d56f6f70aed00efad6cb52addd2863
Khang Thinh Nguyen <khangtnguyen@kms-technology.com>
2020-11-27 10:42:50 +0700

    fix: add activated field to user table

--- migrations/sqls/20201124144615-authentication-up.sql
+++ migrations/sqls/20201124144615-authentication-up.sql
@@ -2,10 +2,8 @@
 CREATE SEQUENCE public.user_id_seq
 	INCREMENT BY 1
 	MINVALUE 1
-	MAXVALUE 2147483647
-	START 1
-	CACHE 1
-	NO CYCLE;
+	MAXVALUE 2147483647 START 1
+	CACHE 1 NO CYCLE;
 
 CREATE TABLE public. "user" (
 	id bigint NOT NULL DEFAULT nextval('user_id_seq'::regclass),
@@ -13,6 +11,7 @@ CREATE TABLE public. "user" (
 	phone character varying(255) COLLATE pg_catalog. "default",
 	salt character varying(255) COLLATE pg_catalog. "default" NOT NULL,
 	hashed_password character varying(512) COLLATE pg_catalog. "default" NOT NULL,
+	activated boolean DEFAULT FALSE,
 	created_date timestamp with time zone NOT NULL DEFAULT now(),
 	modified_date timestamp with time zone NOT NULL DEFAULT now(),
 	CONSTRAINT user_pkey PRIMARY KEY (id))
@@ -24,27 +23,19 @@ ALTER TABLE public. "user" OWNER TO postgres;
 CREATE SEQUENCE public.token_id_seq
 	INCREMENT BY 1
 	MINVALUE 1
-	MAXVALUE 2147483647
-	START 1
-	CACHE 1
-	NO CYCLE;
+	MAXVALUE 2147483647 START 1
+	CACHE 1 NO CYCLE;
 
-CREATE TABLE public.token
-(
-    id bigint NOT NULL DEFAULT nextval('token_id_seq'::regclass),
-    user_id bigint NOT NULL,
-    type character varying(256) COLLATE pg_catalog."default" NOT NULL,
-    token character varying(512) COLLATE pg_catalog."default" NOT NULL,
-    created_date timestamp with time zone NOT NULL DEFAULT now(),
-    modified_date timestamp with time zone NOT NULL DEFAULT now(),
-    CONSTRAINT token_pkey PRIMARY KEY (id),
-    CONSTRAINT token_user_fkey FOREIGN KEY (user_id)
-        REFERENCES public."user" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
+CREATE TABLE public.token (
+	id bigint NOT NULL DEFAULT nextval('token_id_seq'::regclass),
+	user_id bigint NOT NULL,
+	type character varying(256) COLLATE pg_catalog. "default" NOT NULL,
+	token character varying(512) COLLATE pg_catalog. "default" NOT NULL,
+	created_date timestamp with time zone NOT NULL DEFAULT now(),
+	modified_date timestamp with time zone NOT NULL DEFAULT now(),
+	CONSTRAINT token_pkey PRIMARY KEY (id),
+	CONSTRAINT token_user_fkey FOREIGN KEY (user_id) REFERENCES public. "user" (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION)
 TABLESPACE pg_default;
 
-ALTER TABLE public.token
-    OWNER to postgres;
+ALTER TABLE public.token OWNER TO postgres;
+
